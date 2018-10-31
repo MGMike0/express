6f316655e7a6680a3873256837bf458421531f6b
MGMike0 <ingmiguelmorangomez@gmail.com>
2018-10-30 20:50:02 -0600

    clase de hoy

--- app.js
+++ app.js
@@ -1,11 +1,41 @@
 const express = require('express');
 const port = process.env.PORT || 3000;
+const hbs = require('hbs');
 
 const app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+	return new Date().getFullYear()	
+})
+hbs.registerHelper('upperCase', (text) => {
+	return text.toUpperCase()	
+})
 
 app.get('/', async  (req, res) => {
 	res.send('DenmeChamba');
 });
+
+app.get('/about', async  (req, res) => {
+	res.render('about.hbs',{
+		title:'aboutsi',
+		body:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae quisquam nulla veritatis reiciendis.Quas accusantium vitae voluptas architecto, autem veritatis beatae enim, molestias mollitia iure unde itaque dolorum minus veniam.'
+		
+	})
+});
+
+app.get('/contact', async  (req, res) => {
+	res.render('contact.hbs',{
+		title:'Contacto',
+		
+
+	})
+});
+
 
 app.listen(port);
 
